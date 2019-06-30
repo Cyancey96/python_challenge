46f1feff3143ec19c32369536bfe5650f00f0931
Cyancey96 <chris.yancey96@gmail.com>
2019-06-30 17:10:06 -0600

    Add files via upload

---
+++ Files/README.md
@@ -0,0 +1,33 @@
+How to run:
+Put any files you want the program to be able to save/load to in the Files folder.  Run main.py using Python 3.7
+
+Provided Files:
+TESTFILE_IPS.txt - Used by unit tests
+TESTFILE_LOAD.txt - Used by unit tests
+list_of_ips.txt - Text file containing 5000 IPs.  Load IPs into the program with the "Get IPs" button.
+list_of_ips_RESULTS.txt - Text file containing responses for IPs in list_of_ips.txt.  Load into the program using "Load Responses" to skip the IP Lookup process.
+small_list_of_ips.txt - Text file containing 100 IPs.  I recommend using this file if you want to try out IP Lookup.
+
+Navigating the UI:
+Get IPs- Located at the top of the UI.  Clicking this button will check the Files folder for a file with the same name in the box left to the button and populate the IP List with any IPs found in the file.
+IP Lookup- Located at the left of the UI.  Clicking this button will look up Geo IP data for all IPs in the list.  Progress can be checked from the terminal window.  Results are populated in the Responses list.
+Save/Load Responses- 3 buttons located at the bottom of the UI.  "Load Responses" will load a file from the Files folder into the Responses list.  "Save Filtered Responses" will save a list of filtered responses to a file if a filter is applied.  "Save Responses" will save all responses to to a file, including filtered responses.  The file used by these buttons is specified using the textbox above them.
+Filter Responses- Located in the bottom-right of the UI.  Clicking this will filter out responses from the Responses list using a Query provided in the textbox to the left of the button.  These responses can be saved using the "Save Filtered Responses" button.  See below for Query Language details.
+Clear Filter- Located in the bottom-right of the UI.  Clicking this will remove a filter and display the original list of responses in the Responses window.
+
+Query Language:
+Grammar - <key><operator><value>:<key><operator><value>
+':' is used to seperate statements.
+
+Operators:
+'='- Equals, can be used with strings and numbers
+'!='- Not equals, can be used with strings and numbers
+'<'- Less than, can be used with numbers
+'>'- Greater than, can be used with numbers
+'<='- Less than or equal, can be used with numbers
+'>=' Greater than or equal, can be used with numbers
+
+Query Language Examples:
+"ip=1.1.1.1"- Filters responses with an ip of "1.1.1.1"
+"country_code!=JP:latitude<0"- Filters responses with a latitude less than 0 if JP is not their country_code
+"country_code=US:zip_code=:longitude>=0" - Filters responses with a country_code of "US", a blank zip code, and a longitude greater than or equal to 0.
---
+++ Files/main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+from functions import *
+import tkinter as tk
+
+# GUI helper vars
+ip_list = []
+geo_list = []
+filtered_geo_list = []
+
+
+def GUI_parse_file():
+    global filename_entry, ip_list, ip_list_lb
+    ip_list = parse_file(filename_entry.get())
+    '''Update GUI'''
+    ip_list_lb.delete(0, ip_list_lb.size())
+    for i, ip in enumerate(ip_list, 0):
+        ip_list_lb.insert(i, ip)
+
+
+def GUI_geo_lookup_ips():
+    global ip_list, geo_list, geo_ip_results
+    geo_list = geo_lookup_ips(ip_list)
+    '''Update GUI'''
+    for i, geo_data in enumerate(geo_list, 0):
+        geo_ip_results.insert(i, geo_data)
+
+
+def GUI_save_geodata():
+    global geo_list, geo_filename_entry
+    save_geodata(geo_filename_entry.get(), geo_list)
+
+
+def GUI_save_filtered_geodata():
+    global filtered_geo_list, geo_filename_entry
+    save_geodata(geo_filename_entry.get(), filtered_geo_list)
+
+
+def GUI_load_geodata():
+    global geo_list, geo_ip_results, geo_filename_entry
+    geo_list = load_geodata(geo_filename_entry.get())
+    '''Update GUI'''
+    geo_ip_results.delete(0, geo_ip_results.size())
+    for i, geo_data in enumerate(geo_list, 0):
+        geo_ip_results.insert(i, geo_data)
+
+
+def GUI_filter_response():
+    global geo_list, filtered_geo_list, geo_ip_results, filter_entry
+    filtered_geo_list = filter_response(geo_list, filter_entry.get())
+    '''Update GUI'''
+    geo_ip_results.delete(0, geo_ip_results.size())
+    for i, geo_data in enumerate(filtered_geo_list, 0):
+        geo_ip_results.insert(i, geo_data)
+
+
+def GUI_clear_filter():
+    global geo_list, filtered_geo_list, geo_ip_results
+    filtered_geo_list = []
+    '''Update GUI'''
+    geo_ip_results.delete(0, geo_ip_results.size())
+    for i, geo_data in enumerate(geo_list, 0):
+        geo_ip_results.insert(i, geo_data)
+
+
+top = tk.Tk()
+root = tk.Frame(top)
+root.pack(side=tk.TOP)
+file_label = tk.Label(root, text="File Name:")
+file_label.pack(side=tk.LEFT)
+filename_entry = tk.Entry(root, bd=5)
+filename_entry.pack(side=tk.LEFT)
+get_ip_button = tk.Button(root, text="Get IPs", command=GUI_parse_file)
+get_ip_button.pack(side=tk.LEFT)
+ip_frame = tk.Frame(top)
+ip_frame.pack(side=tk.LEFT)
+res_frame = tk.Frame(top)
+res_frame.pack(side=tk.LEFT)
+filter_frame = tk.Frame(top)
+filter_frame.pack(side=tk.BOTTOM)
+filter_label = tk.Label(filter_frame, text="Query:")
+filter_label.pack(side=tk.LEFT)
+filter_entry = tk.Entry(filter_frame, bd=5)
+filter_entry.pack(side=tk.LEFT)
+filter_button = tk.Button(filter_frame, text="Clear Filter", command=GUI_clear_filter)
+filter_button.pack(side=tk.BOTTOM)
+filter_button = tk.Button(filter_frame, text="Filter Responses", command=GUI_filter_response)
+filter_button.pack(side=tk.BOTTOM)
+load_button = tk.Button(res_frame, text="Load Responses", command=GUI_load_geodata)
+load_button.pack(side=tk.BOTTOM)
+save_button = tk.Button(res_frame, text="Save Responses", command=GUI_save_geodata)
+save_button.pack(side=tk.BOTTOM)
+save_filtered_button = tk.Button(res_frame, text="Save Filtered Responses", command=GUI_save_filtered_geodata)
+save_filtered_button.pack(side=tk.BOTTOM)
+geo_filename_entry = tk.Entry(res_frame, bd=5)
+geo_filename_entry.pack(side=tk.BOTTOM)
+file_label = tk.Label(res_frame, text="File Name:")
+file_label.pack(side=tk.BOTTOM)
+geo_ip_results = tk.Listbox(res_frame, height=20, width=100)
+geo_ip_results.pack(side=tk.BOTTOM)
+ips_label = tk.Label(ip_frame, text="IP List")
+ips_label.pack(side=tk.TOP)
+ips_label = tk.Label(res_frame, text="Responses")
+ips_label.pack(side=tk.TOP)
+ip_list_lb = tk.Listbox(ip_frame, height=20)
+ip_list_lb.pack(side=tk.TOP)
+geo_lookup_button = tk.Button(ip_frame, text="IP Lookup", command=GUI_geo_lookup_ips)
+geo_lookup_button.pack(side=tk.BOTTOM)
+top.mainloop()
